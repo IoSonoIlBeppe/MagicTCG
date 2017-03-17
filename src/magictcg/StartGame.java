3e417fbaf190b7ac034055870646f4d57c6b8ac8
gianmarcocallegher <gianmarcocallegher@157.138.184.134>
2017-03-17 11:47:20 +0100

    Creato mazzo Omeopathy Modificato Player Sistemato all'originale PhaseManager Creato uno StartGame di Prova

--- src/magictcg/StartGame.java
+++ src/magictcg/StartGame.java
@@ -5,8 +5,6 @@
  */
 package magictcg;
 
-import java.util.ArrayList;
-import magictcg.player.Deck;
 import magictcg.player.Player;
 
 /**
@@ -14,26 +12,25 @@ import magictcg.player.Player;
  * @author gianmarcocallegher
  */
 public class StartGame {
+
     private final static int LIFEPOINTS = 20;
-    
-    public static void main(String[] args) {       
+
+    public static void main(String[] args) {
         Game g = Game.getInstanceGame();
+        System.out.println("creato il game");
         g.setP1(new Player(LIFEPOINTS, "Player1"));
         g.setP2(new Player(LIFEPOINTS, "Player2"));
         g.setStack(new EffectStack());
         g.setCurrentplayer(g.getP1());
         g.getP1().setFirstHand();
         g.getP2().setFirstHand();
-        while (g.getP1().getLifepoints()>0 && g.getP2().getLifepoints()>0){
-            Player p=g.getCurrentplayer();
-            System.out.println("È il turno del giocatore "+ g.getCurrentplayer().getName());
-            PhaseManager m=p.getPhaseManager();
+        while (g.getP1().getLifepoints() > 0 && g.getP2().getLifepoints() > 0) {
+            Player p = g.getCurrentplayer();
+            System.out.println("È il turno del giocatore " + g.getCurrentplayer().getName());
+            PhaseManager m = p.getPhaseManager();
             m.resetPhases();
             m.resolvePhases();
         }
-        
-        
-        
-    
+
     }
 }
