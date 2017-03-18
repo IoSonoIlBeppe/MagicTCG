d82971bc60625369d140a8316f1cce0fff709cf1
gianmarcocallegher <gianmarcocallegher@157.138.184.134>
2017-03-18 11:13:50 +0100

    Commentato deck

--- src/magictcg/player/Deck.java
+++ src/magictcg/player/Deck.java
@@ -21,15 +21,25 @@ public class Deck {
         this.magics = new ArrayList<>();
     }
     
-    // NOTA BENE solo per debug
-    public void push(IMagic card) {
-        magics.add(card);
+    /**
+     * Mette una magia m nel mazzo
+     * @param m magia da inserire nel deck
+     */
+    public void push(IMagic m) {
+        magics.add(m);
     }
-    /* Pesca una magia dal Deck, se non è vuoto */
+    /**
+     * Pesca una magia dal Deck, se non è vuoto
+     * @return ritorna la magia in cima al deck
+     */
     public IMagic pop () {
         return (magics.isEmpty())? null : magics.remove(0);
     }
     
+    /**
+     * Computa il numero di magie nel deck
+     * @return la dimensione del deck
+     */
     public int getSize() {
         return magics.size();
     }
